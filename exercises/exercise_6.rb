3421b5f64b9416b03cf32a10ff205381d473ded9
mxtchjohnston <mitchjohnston99@gmail.com>
2023-11-13 13:35:47 -0800

    exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.employees.create(first_name: "Mitch", last_name: "Johnston", hourly_rate: 60)
+@store2.employees.create(first_name: "Steve", last_name: "Fraser", hourly_rate: 60)
+
+Employee.all.each do |emp|
+  puts "#{emp.first_name} #{emp.last_name} #{emp.hourly_rate} at store #{emp.store_id}"
+end
