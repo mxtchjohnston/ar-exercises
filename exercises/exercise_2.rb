8951fddd5816b67f4b9b35cd3071aa12ed4a38b3
mxtchjohnston <mitchjohnston99@gmail.com>
2023-11-13 12:57:41 -0800

    exercise 2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+begin
+  store1 = Store.find(1)
+  store2 = Store.find(2)
+  store1.update(name: "Burquitlam")
+rescue ActiveRecord::RecordNotFound
+  puts "Store not found"
+end
+
+p store1
